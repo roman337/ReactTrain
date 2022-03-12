fcd0d3764e7a32647ea39607d7021708d4368ffd
Roman Sviridov <roman123456789062819@mail.ru>
2022-03-12 14:52:37 +0300

    small navbar is present1

--- src/UI/Navbar/Navbar.jsx
+++ src/UI/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navbar__links">
+
                 <Link to="/about">О сайте</Link>
                 <Link to="/posts">Посты</Link>
             </div>
